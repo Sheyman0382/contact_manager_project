9afaa3ca187654b0760a969aacee6290359990f2
Sheyman0382 <adegbiteseyi22@gmail.com>
2026-05-09 14:56:51 +0000

    contact.py implemented. This include a contact class with the __init__(), then save_to_file method implemented

---
+++ contact.py
@@ -0,0 +1,52 @@
+#!/usr/bin/ppython3
+"""Contact manager module"""
+import csv
+import os
+
+
+class Contact:
+    """contanct manager class"""
+
+    def __init__(self, name, phone_number):
+        self.name = name
+        self.phone_number = phone_number
+        
+    @property
+    def name(self):
+
+        """beyond just initializing, controls hw name is used"""
+        return self.__name
+    
+    @name.setter
+    def name(self, value):
+        if not type(value) is str:
+            raise TypeError("name must be a string")
+        self.__name = value
+        
+    @property
+    def phone_number(self):
+        """controls internally how phone number behaves"""
+        return self.__phone_number
+        
+    @phone_number.setter
+    def phone_number(self, value):
+        if not type(value) is str:
+            raise TypeError("phone_number must be a string")
+        if len(value) != 11:
+            raise ValueError("mobile number must be equal to 11 digits")
+        self.__phone_number = value
+
+    def __str__(self):
+        return "{} \n{}".format(self.name, self.phone_number)
+        
+    def to_dictionary(self):
+        return {"name": self.name, "phone_number": self.phone_number} 
+
+    def save_to_file(self):
+        """a method that writes into a csv file"""
+
+        with open("contacts.csv", "a", newline="") as csv_file:
+             writer = csv.writer(csv_file)
+            if not os.stat(contacts.csv).st_size:
+                writer.writerow(["Name", "Phone_number"])
+            writer.writerow([self.name, self.phone_number])
