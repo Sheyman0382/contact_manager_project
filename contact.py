fd4c4404f2ad2d8d314a279b6cae116525096dd9
Sheyman0382 <adegbiteseyi22@gmail.com>
2026-05-15 21:59:10 +0000

    every methods that handle file management moved to a new file called database.py with a new method called add_contact.Now that separates adding or call it appeding a new contact from savind the contact. also load_from_file and save_to_file has been renamed as load_contacts and save_contacts

--- contact.py
+++ contact.py
@@ -20,7 +20,7 @@ class Contact:
 
     @name.setter
     def name(self, value):
-        if not type(value) is str:
+        if not isinstance(value, str):
             raise TypeError("name must be a string")
         self.__name = value
 
@@ -33,7 +33,7 @@ class Contact:
     def phone_number(self, value):
         if not type(value) is str:
             raise TypeError("phone_number must be a string")
-        if len(value) != 11:
+        if len(value) != 11 or not value.isdigit():
             raise ValueError("mobile number must be equal to 11 digits")
         self.__phone_number = value
 
@@ -50,71 +50,3 @@ class Contact:
             dictionary["name"],
             dictionary["phone_number"]
         )
-
-    def save_to_file(self):
-        """a method that writes into a csv file"""
-
-        new_file_content = []
-        if os.stat(self.FILE_NAME).st_size:
-            file_content = self.load_from_file()
-            for obj in file_content:
-                new_file_content.append(obj.to_dictionary())
-        new_file_content.append(self.to_dictionary())
-        with open(self.FILE_NAME, "w", newline="") as csv_file:
-            writer = csv.DictWriter(
-                csv_file,
-                fieldnames=["name", "phone_number"]
-            )
-            writer.writeheader()
-            for obj in new_file_content:
-                writer.writerow(obj)
-            print("contact saved successfully")
-
-    @classmethod
-    def load_from_file(cls):
-        """returns a list of references to object(s) as the case may be"""
-        if not os.path.exists(cls.FILE_NAME):
-            return []
-        if not os.stat(cls.FILE_NAME).st_size:
-            return []
-        rows = []
-        with open(cls.FILE_NAME, newline="") as csv_file:
-            reader = csv.DictReader(csv_file)
-            for row in reader:
-                rows.append(cls.from_dictionary(row))
-        return rows
-
-    @classmethod
-    def search_contact(cls, contact_name):
-        """searches if a contact exists of not"""
-        file_content = cls.load_from_file()
-        if file_content == []:
-            return "contact list is empty"
-        for obj in file_content:
-            if obj.name == contact_name:
-                return obj
-        return "contact not found"
-
-    @classmethod
-    def delete_contact(cls, contact_name):
-        """to delete unwanted contact"""
-
-        file_content = cls.load_from_file()
-        new_file_content = []
-        deleted = False
-        for obj in file_content:
-            if obj.name == contact_name:
-                deleted = True
-            else:
-                new_file_content.append(obj.to_dictionary())
-        if not deleted:
-            print("contact not found")
-            return
-        with open(cls.FILE_NAME, "w", newline="") as csv_file:
-            writer = csv.DictWriter(
-                csv_file,
-                fieldname=["name", "phone_number"]
-            )
-            writer.writeheader()
-            for obj in new_file_content:
-                writer.writerow(obj)
---
+++ database.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+from contact import Contact
+import os
+import csv
+
+
+class ContactDatabase:
+    """this class manages every methods involved with database"""
+    FILE_NAME = "contacts.csv"
+
+    @classmethod
+    def load_contacts(cls):
+        """returns a list of references to object(s) as the case may be"""
+        if not os.path.exists(cls.FILE_NAME):
+            return []
+        if not os.stat(cls.FILE_NAME).st_size:
+            return []
+        rows = []
+        with open(cls.FILE_NAME, newline="") as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                rows.append(Contact.from_dictionary(row))
+        return rows
+
+    @classmethod
+    def save_contacts(cls, contacts):
+        """a method that writes into a csv file"""
+
+        new_file_content = []
+        for obj in contacts:
+            new_file_content.append(obj.to_dictionary())
+        with open(cls.FILE_NAME, "w", newline="") as csv_file:
+            writer = csv.DictWriter(
+                csv_file,
+                fieldnames=["name", "phone_number"]
+            )
+            writer.writeheader()
+            writer.writerows(new_file_content)
+            print("contact saved successfully")
+
+    @classmethod
+    def add_contact(cls, contact):
+        """add/maybe append a contact into the list"""
+        if not isinstance(contact, Contact):
+            raise TypeError("only contact object is alowed")
+        new_file_content = cls.load_contacts()
+        new_file_content.append(contact)
+        cls.save_contacts(new_file_content)
+
+    @classmethod
+    def delete_contact(cls, contact_name):
+        """to delete unwanted contact"""
+
+        file_content = cls.load_contacts()
+        new_file_content = []
+        deleted = False
+        for obj in file_content:
+            if obj.name == contact_name:
+                deleted = True
+            else:
+                new_file_content.append(obj)
+        if not deleted:
+            return False
+        cls.save_contacts(new_file_content)
+        return True
+
+    @classmethod
+    def search_contact(cls, contact_name):
+        """searches if a contact exists of not"""
+        file_content = cls.load_contacts()
+        if not file_content:
+            return None
+        for obj in file_content:
+            if obj.name == contact_name:
+                return obj
+        return None
